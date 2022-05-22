cde6cedb2a130e1dd388180f552c11c59cbfd39b
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-05-23 02:13:06 +0600

    all products GET is implemented from DB

--- src/components/Manage/Manageproduct.js
+++ src/components/Manage/Manageproduct.js
@@ -7,7 +7,7 @@ const Manageproduct = () => {
     const [products, setProducts] = useState([])
 
     useEffect(() => {
-        fetch('data.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => console.log(data))
     }, [])
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -9,7 +9,7 @@ const Products = () => {
     const navigate = useNavigate()
 
     useEffect(() => {
-        fetch('data.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
